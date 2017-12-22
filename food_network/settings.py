fa3654c6ad53b3103afc0a209cd80aaed3d24375
NgocTP <phuongngoc@gmail.com>
2017-12-22 07:12:40 +0700

    Add configuration to connect MySQL

--- food_network/settings.py
+++ food_network/settings.py
@@ -78,6 +78,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        # add for mysql
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'mydatabase',
+        # 'USER': 'mydatabaseuser',
+        # 'PASSWORD': 'mypassword',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '3306',
     }
 }
 
